3ba341a991ee3aa6488c3a4770413f7bd0f7374c
Yacine Filali <yacine@pit.ai>
2018-07-11 18:29:24 -0700

    Renaming setup functions

--- python/setup.py
+++ python/setup.py
@@ -17,7 +17,7 @@ except:
     readme = ''
 
 
-class install_pit_dependencies():
+class install_devise_dependencies():
     print("Installing platform specific wheels...")
     try:
         # For windows, try to install one of the pre-built binary wheels
@@ -36,42 +36,42 @@ class install_pit_dependencies():
         [sys.executable, '-m', 'pip', 'install', 'deps/blue_loader_python.tgz'])
 
 
-def pit_setup(**kwargs):
-    install_pit_dependencies()
+def devise_setup(**kwargs):
+    install_devise_dependencies()
     setup(**kwargs)
 
 
-pit_setup(name='devise',
-          maintainer='Devise Foundation',
-          version='1.3',
-          license='GPL-3',
-          description='Devise: An Ethereum Marketplace for Engineering Better Representations of Financial Markets',
-          url='https://github.com/devisechain/devise',
-          long_description=readme,
-          packages=find_packages(exclude=['tests']),
-          include_package_data=True,
-          install_requires=[
-              'web3==4.2.1',
-              'rlp==0.6.0',
-              'pysha3==1.0.2'
-          ],
-          extras_require={
-              'dev': [
-                  'pytest',
-                  'pep8',
-                  'pylint',
-                  'pytest-cov'
-              ]
-          },
-          classifiers=[
-              'Development Status :: 4 - Beta',
-              'Environment :: Console',
-              'Intended Audience :: Developers',
-              'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-              'Operating System :: OS Independent',
-              'Programming Language :: Python',
-              'Programming Language :: Python :: 3.5',
-              'Programming Language :: Python :: 3.6',
-              'Topic :: Software Development :: Libraries',
-              'Topic :: Office/Business :: Financial :: Investment'
-          ])
+devise_setup(name='devise',
+             maintainer='Devise Foundation',
+             version='1.3',
+             license='GPL-3',
+             description='Devise: An Ethereum Marketplace for Engineering Better Representations of Financial Markets',
+             url='https://github.com/devisechain/devise',
+             long_description=readme,
+             packages=find_packages(exclude=['tests']),
+             include_package_data=True,
+             install_requires=[
+                 'web3==4.2.1',
+                 'rlp==0.6.0',
+                 'pysha3==1.0.2'
+             ],
+             extras_require={
+                 'dev': [
+                     'pytest',
+                     'pep8',
+                     'pylint',
+                     'pytest-cov'
+                 ]
+             },
+             classifiers=[
+                 'Development Status :: 4 - Beta',
+                 'Environment :: Console',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Topic :: Software Development :: Libraries',
+                 'Topic :: Office/Business :: Financial :: Investment'
+             ])
